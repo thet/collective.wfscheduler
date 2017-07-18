abe33da568bfd3aae79c1cd481a3e30f8ac2cbd6
Johannes Raggam <thetetet@gmail.com>
2017-07-18 16:15:25 +0200

    add filter + task_active column

--- src/collective/wfscheduler/browser/wftaskfolder.py
+++ src/collective/wfscheduler/browser/wftaskfolder.py
@@ -8,13 +8,33 @@ import plone.api
 
 class WFTaskFolderView(FolderView):
 
+    def results(self, **kwargs):
+        # Default is to show all.
+        kwargs['portal_type'] = 'WFTask'
+        filter_ = self.request.form.get('filter')
+        if filter_ == 'active':
+            kwargs['is_active'] = True
+        elif filter_ == 'inactive':
+            kwargs['is_active'] = False
+        return super(WFTaskFolderView, self).results(**kwargs)
+
+    @property
+    def filter_selected(self):
+        filter_ = self.request.form.get('filter')
+        return {
+            'active': ' selected' if filter_ == 'active' else '',
+            'inactive': ' selected' if filter_ == 'inactive' else '',
+            'all': ' selected' if filter_ not in ('active', 'inactive') else ''
+        }
+
     @property
     def tabular_fields(self):
         return [
             'title',
             'task_action',
             'task_items',
-            'task_date'
+            'task_date',
+            'task_active'
         ]
 
     def tabular_fielddata(self, item, fieldname):
